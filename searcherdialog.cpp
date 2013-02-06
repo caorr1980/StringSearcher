482c9d7412cfcd0427b58e653aa9af381abd3a10
Cao Rongrong <caorr1980@163.com>
2013-02-06 18:29:08 +0800

    Basic string search is ready.

--- searcherdialog.cpp
+++ searcherdialog.cpp
@@ -8,10 +8,15 @@ SearcherDialog::SearcherDialog(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    /* set default directory to search */
+    ui->CMB_DirPath->setEditText(QDir::currentPath());
+
     /* rename TBW_Result's horizontal header */
     QStringList headers;
-    headers << tr("File");
+    headers << tr("File") << tr("Line") << tr("Context");
     ui->TBW_Result->setHorizontalHeaderLabels(headers);
+    ui->TBW_Result->setColumnWidth(0, 300);
+    ui->TBW_Result->setColumnWidth(1, 60);
 }
 
 SearcherDialog::~SearcherDialog()
@@ -29,8 +34,7 @@ void SearcherDialog::on_BTN_Browse_clicked()
                                 ui->CMB_DirPath->currentText(),
                                 options);
     if (!dirPath.isEmpty()) {
-        updateComboBox(ui->CMB_DirPath, dirPath);
-        ui->CMB_DirPath->setCurrentIndex(ui->CMB_DirPath->findText(dirPath));
+        ui->CMB_DirPath->setEditText(dirPath);
     }
 }
 
@@ -40,46 +44,106 @@ void SearcherDialog::on_BTN_Search_clicked()
     if (key.isEmpty()) {
         QMessageBox::warning(this, tr("No KeyWord"),
                          tr("Please specify the Keyword to search!"));
-        qDebug() << "No KeyWord";
         return;
     }
+    /* save history for each ComboBox */
+    updateComboBox(ui->CMB_Keyword, key);
+    QString replace = ui->CMB_Replace->currentText();
+    updateComboBox(ui->CMB_Replace, replace);
+    QString filter = ui->CMB_Filter->currentText();
+    updateComboBox(ui->CMB_Filter, filter);
+
+    if (filter.isEmpty())
+        filter = "*";
 
     /* truncate previous results */
+    ui->TBW_Result->clearContents();
     ui->TBW_Result->setRowCount(0);
 
-    QString fileFilter = ui->CMB_Filter->currentText();
-    if (fileFilter.isEmpty())
-        fileFilter = "*";
+    fileFoundNum = 0;
 
     QString path = ui->CMB_DirPath->currentText();
-    QDir curDir = QDir(path);
-    QStringList files = curDir.entryList(QStringList(fileFilter),
-                                 QDir::Files | QDir::NoSymLinks);
-//    if (!text.isEmpty())
-//        files = findFiles(files, text);
-    showFiles(files);
+    updateComboBox(ui->CMB_DirPath, path);
+
+    searchDir = QDir(path);
+    searchDirectory(searchDir, filter, key);
+
+    ui->LBL_number->setText(tr("%1 file(s) found").arg(fileFoundNum));
+    ui->LBL_number->setWordWrap(true);
+}
+
+void SearcherDialog::searchDirectory(QDir &dir, QString &filter, QString &key)
+{
+    QStringList fileList = dir.entryList(QStringList(filter),
+                       QDir::Files | QDir::NoSymLinks | QDir::Dirs | QDir::NoDotAndDotDot);
+
+    for (int i = 0; i < fileList.size(); i++) {
+        QString filePath = dir.absoluteFilePath(fileList[i]);
+        QFileInfo info(filePath);
+        if (info.isDir()) {
+            QDir subdir(filePath);
+            searchDirectory(subdir, filter, key);
+        } else {
+            if (searchString(filePath, key))
+                fileFoundNum++;
+        }
+    }
 }
 
 void SearcherDialog::updateComboBox(QComboBox *comboBox, QString &text)
 {
+    if (text.isEmpty())
+        return;
+
     if (comboBox->findText(text) == -1)
         comboBox->insertItem(0, text);
 }
 
-void SearcherDialog::showFiles(const QStringList &files)
+bool SearcherDialog::searchString(const QString &filePath, const QString &key, SearchMode md)
 {
-    int row;
+    bool found = false;
+    QFile file(filePath);
 
-    for (int i = 0; i < files.size(); i++) {
-        qDebug() << files[i];
-        QTableWidgetItem *fileItem = new QTableWidgetItem(files[i]);
-        fileItem->setFlags(fileItem->flags() ^ Qt::ItemIsEditable);
-
-        row = ui->TBW_Result->rowCount();
-        ui->TBW_Result->insertRow(row);
-        ui->TBW_Result->setItem(row, 0, fileItem);
+    if (!file.open(QIODevice::ReadOnly)) {
+        return false;
     }
 
-    ui->LBL_number->setText(tr("%1 file(s) found").arg(files.size()));
-    ui->LBL_number->setWordWrap(true);
+    QTextStream in(&file);
+    QString text;
+    int line = 0;
+
+    while(!in.atEnd()) {
+        line++;
+        if (md == QTMode) {
+            text = in.readLine();
+            if (text.contains(key)) {
+                int index = text.indexOf(key);
+                QString context = text.right(index - 20);
+                showResult(filePath, line, context);
+                found = true;
+                break;
+            }
+        }
+    }
+
+    return found;
+}
+
+void SearcherDialog::showResult(const QString &filePath, const int &line, const QString &context)
+{
+    int row = ui->TBW_Result->rowCount();
+    ui->TBW_Result->insertRow(row);
+
+    QTableWidgetItem *item0 = new QTableWidgetItem(searchDir.relativeFilePath(filePath));
+    item0->setFlags(item0->flags() ^ Qt::ItemIsEditable);
+    ui->TBW_Result->setItem(row, 0, item0);
+
+    QTableWidgetItem *item1 = new QTableWidgetItem(tr("%1").arg(line));
+    item1->setTextAlignment(Qt::AlignHCenter);
+    item1->setFlags(item1->flags() ^ Qt::ItemIsEditable);
+    ui->TBW_Result->setItem(row, 1, item1);
+
+    QTableWidgetItem *item2 = new QTableWidgetItem(context);
+    item2->setFlags(item2->flags() ^ Qt::ItemIsEditable);
+    ui->TBW_Result->setItem(row, 2, item2);
 }
--- searcherdialog.h
+++ searcherdialog.h
@@ -14,6 +14,11 @@ class SearcherDialog : public QDialog
 public:
     explicit SearcherDialog(QWidget *parent = 0);
     ~SearcherDialog();
+
+    enum SearchMode {
+        QTMode       = 0x0001,
+        BMMode       = 0x0002,
+    };
     
 private slots:
     void on_BTN_Browse_clicked();
@@ -24,7 +29,12 @@ private:
     Ui::SearcherDialog *ui;
 
     void updateComboBox(QComboBox *comboBox, QString &text);
-    void showFiles(const QStringList &files);
+    void searchDirectory(QDir &dir, QString &filter, QString &key);
+    bool searchString(const QString &filePath, const QString &key, SearchMode md = QTMode);
+    void showResult(const QString &filePath, const int &line, const QString &context);
+
+    QDir searchDir;
+    int fileFoundNum;
 };
 
 #endif // SEARCHERDIALOG_H
